f5d60c6104c52e2570d5105669a625e06f8d5005
Ivan_Andrianov <andreyanovi@yandex.ru>
2024-11-15 20:58:30 +0300

    loading all images into variables

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+
+back = pygame.image.load('code_game_back_floor.jpg')
+hero = pygame.image.load('detective.png')
+pot = pygame.image.load('teapot.png')
+candle = pygame.image.load('candlestick.png')
+box = pygame.image.load('wooden_box.png')
